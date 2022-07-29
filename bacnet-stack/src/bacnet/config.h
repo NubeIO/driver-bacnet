ce91ed5f6fd66cc91d6eed92fc7be72b5cd57de7
Sho <sho.maglasang@gmail.com>
2022-07-29 17:38:01 +0000

    Issue-13: Change Bacnet vendor ID and vendor name

--- bacnet-stack/src/bacnet/config.h
+++ bacnet-stack/src/bacnet/config.h
@@ -70,10 +70,10 @@
 
 /* Define your Vendor Identifier assigned by ASHRAE */
 #if !defined(BACNET_VENDOR_ID)
-#define BACNET_VENDOR_ID 260
+#define BACNET_VENDOR_ID 1173
 #endif
 #if !defined(BACNET_VENDOR_NAME)
-#define BACNET_VENDOR_NAME "BACnet Stack at SourceForge"
+#define BACNET_VENDOR_NAME "Nube iO Operations Pty Ltd"
 #endif
 
 /* Max number of bytes in an APDU. */
